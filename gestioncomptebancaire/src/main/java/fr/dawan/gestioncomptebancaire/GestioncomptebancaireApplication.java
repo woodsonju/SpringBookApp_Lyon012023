c9f1ad7526ee975558bf0f3a431870fb4f3bdd02
Woodson Juste <j.woodson@hotmail.fr>
2023-01-26 17:07:02 +0100

    consultercompte, saveOperation (controller + vues)

--- gestioncomptebancaire/src/main/java/fr/dawan/gestioncomptebancaire/GestioncomptebancaireApplication.java
+++ gestioncomptebancaire/src/main/java/fr/dawan/gestioncomptebancaire/GestioncomptebancaireApplication.java
@@ -1,13 +1,64 @@
 package fr.dawan.gestioncomptebancaire;
 
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
+
+import fr.dawan.gestioncomptebancaire.dao.ClientRepository;
+import fr.dawan.gestioncomptebancaire.dao.CompteRepository;
+import fr.dawan.gestioncomptebancaire.dao.OperationRepository;
+import fr.dawan.gestioncomptebancaire.entities.Client;
+import fr.dawan.gestioncomptebancaire.entities.Compte;
+import fr.dawan.gestioncomptebancaire.entities.CompteCourant;
+import fr.dawan.gestioncomptebancaire.entities.CompteEpargne;
+import fr.dawan.gestioncomptebancaire.entities.Retrait;
+import fr.dawan.gestioncomptebancaire.entities.Versement;
 
 @SpringBootApplication
-public class GestioncomptebancaireApplication {
+public class GestioncomptebancaireApplication implements CommandLineRunner{
+	
+	@Autowired
+	private ClientRepository clientRepository;
+	
+	@Autowired
+	private CompteRepository compteRepository;
+	
+	@Autowired
+	private OperationRepository operationRepository;
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestioncomptebancaireApplication.class, args);
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Client c1 = clientRepository.save(new Client("Mathéo", "matheo@gmail.com"));
+		Client c2 = clientRepository.save(new Client("Louis", "louis@gmail.com"));
+
+		Compte cb1 = compteRepository.save(new CompteCourant("c1", LocalDate.now(), 300000, c1, 2000));
+		Compte cb2 = compteRepository.save(new CompteEpargne("c2", LocalDate.now(), 7000, c2, 5.5));
+
+		operationRepository.save(new Versement(LocalDate.now(), 72000, cb1));
+		operationRepository.save(new Versement(LocalDate.now(), 28000, cb1));
+		operationRepository.save(new Retrait(LocalDate.now(), 30000, cb1));
+		
+		operationRepository.save(new Versement(LocalDate.now(), 1500, cb2));
+		operationRepository.save(new Retrait(LocalDate.now(), 1000, cb2));
+
 		
 	}
 	
--- gestioncomptebancaire/src/main/java/fr/dawan/gestioncomptebancaire/web/BankController.java
+++ gestioncomptebancaire/src/main/java/fr/dawan/gestioncomptebancaire/web/BankController.java
@@ -2,24 +2,72 @@ package fr.dawan.gestioncomptebancaire.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import fr.dawan.gestioncomptebancaire.entities.Compte;
 import fr.dawan.gestioncomptebancaire.service.IBankService;
+import fr.dawan.gestioncomptebancaire.service.exception_.CompteIntrouvableException;
+import fr.dawan.gestioncomptebancaire.service.exception_.SoldeInsuffisantException;
+import fr.dawan.gestioncomptebancaire.service.exception_.VirementMemeCompteException;
 
 //Un controller c'est une classe qui contient l'annotation @Controller
 @Controller
 @RequestMapping("/bank")
 public class BankController {
-	
+
 	@Autowired
 	private IBankService bankService;
-	
+
 	@GetMapping("/operations")
 	public String index() {
 		return "comptes";
 	}
+
+	@GetMapping("/consultercompte")
+	public String consulterCompte(Model model, String numCompte) {
+		Compte cb;
+		try {
+			cb = bankService.consulterCompte(numCompte);
+			model.addAttribute("compte", cb);
+
+		} catch (CompteIntrouvableException e) {
+
+			e.printStackTrace();
+			model.addAttribute("compteIntrouvableException", e.getCompteIntrouvable());
+		}
+
+		return "comptes";
+	}
+
+
+	@PostMapping("/saveOperation")
+	public String saveOperation(Model model, String typeOperation, String numCompte, double montant, 
+			String numCompte2) {
+
+		try {
+			if(typeOperation.equals("versement")) {
+				bankService.verser(numCompte2, montant);
+			} else if(typeOperation.equals("retrait")){
+				bankService.retirer(numCompte2, montant);
+			} else if(typeOperation.equals("virement")) {
+				bankService.virement(numCompte, numCompte2, montant);
+			}
+		} catch (CompteIntrouvableException e) {
+			
+			e.printStackTrace();
+		} catch (SoldeInsuffisantException e) {
 	
-	
+			e.printStackTrace();
+		} catch (VirementMemeCompteException e) {
+		
+			e.printStackTrace();
+		}
+		
+		return null;
+
+	}
 
 }
